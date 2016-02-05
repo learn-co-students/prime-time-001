7654dcb6234fdf7c893f834e3ba51e7b7523e63b
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-05 10:25:44 -0500

    Creates table view to display primes 2000 through 2099

---
+++ PrimeTime/FISPrimeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISPrimeTableViewController.h
+//  PrimeTime
+//
+//  Created by Ken M. Haggerty on 2/5/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISPrimeTableViewController : UITableViewController
+
+@end
